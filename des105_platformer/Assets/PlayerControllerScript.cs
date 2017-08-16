7be96dee252da7bb8686d2c6c67c9c0defe7415a
Christopher Acornley <c516792@uad.ac.uk>
2017-08-16 10:52:00 +0100

    Added basic scene, animated robot and basic left/right controls

---
+++ des105_platformer/Assets/PlayerControllerScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerScript : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    // Called at set interval each time. Good for physics
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+        anim.SetFloat("speed", Mathf.Abs(move));
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
